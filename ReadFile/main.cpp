ac6a52fb54cf39bc7a38ba09492caed4af911122
Kirlen <garciandy579@gmail.com>
2023-10-21 23:28:01 -0400

    Agregada documentacion doxygen al main.cpp

--- ReadFile/main.cpp
+++ ReadFile/main.cpp
@@ -1,17 +1,21 @@
-#include <iostream>
-#include <fstream> // for ifstream & ofstream
-#include <string> // for string object container
+#include <iostream>     // for cout 
+#include <fstream>      // for ifstream & ofstream
+#include <string>       // for string object container
 
-// String Version
+/**
+*  @brief Load a input file path into an string (file not binary)
+*  @param filepath the file path you want read 
+*/
 std::string readFile(std::string filepath){
     std::ifstream file{filepath}; // automatic open file
     // return an string with the file content
     return std::string{
-        std::istreambuf_iterator<char>{ file }, // start into file begin
-        std::istreambuf_iterator<char>{} // automatic end detection (EOF)
+        // start into file begin
+        std::istreambuf_iterator<char>{ file },
+        // automatic end detection (EOF)
+        std::istreambuf_iterator<char>{}
     };
 }
-
 
 int main(){
     std::cout << readFile("./fileexample.txt") << "\n";
